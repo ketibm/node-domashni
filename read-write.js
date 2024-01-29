8932bda79a0d292f4a1875a13ad24fe2069105a4
ketibm <146774511+ketibm@users.noreply.github.com>
2024-01-29 11:59:43 +0100

    Update read-write.js

--- read-write.js
+++ read-write.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const read = async (fileName, data) => {
+const read = async (fileName) => {
   return new Promise((resolve, reject) => {
     fs.readFile(fileName, "utf-8", (err, data) => {
       if (err) return reject(err);
